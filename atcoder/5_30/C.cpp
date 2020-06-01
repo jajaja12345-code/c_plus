e4c0789991422a4ffdb97354480c21952adff8e0
jajaja12345 <vie.estbeats@gmail.com>
2020-06-02 02:08:14 +0900

    5_30 C

--- atcoder/5_30/C.cpp
+++ atcoder/5_30/C.cpp
@@ -5,9 +5,7 @@
 #include <math.h>
 #include <map>
 
-
-
-//////////////////////////////////////////////できてない
+//////////////////////////////////////////////TLE
 
 typedef long long LL;
 
@@ -21,33 +19,40 @@ bool check = 0;
 void xx(vector<LL> &A, vector<LL> &x, int count)
 {
 
-    if (check == 1)
+    if (check == 1 || count > N)
     {
         return;
     }
 
     LL temp = x.at(count - 1) * 2;
+    LL sum_A = 0;
+    for (int i = count; i <= N; i++)
+    {
+        sum_A += A.at(i);
+    }
     while (temp >= x.at(count - 1))
     {
-        if (temp - A.at(count) > 0)
+
+        if (temp - A.at(count) >= 0 && check == 0)
         {
-            if (count == N)
+            if (temp <= sum_A)
             {
-                if (temp - A.at(count) == x.at(count))
+                if (count == N && temp - A.at(count) == x.at(count))
                 {
+
                     check = 1;
                     return;
                 }
-            }
-            else
-            {
-                x.at(count) = temp - A.at(count);
-                xx(A, x, count + 1);
+                else
+                {
+                    x.at(count) = temp - A.at(count);
+                    xx(A, x, count + 1);
+                }
             }
         }
         else
         {
-            break;
+            return;
         }
         temp--;
     }
@@ -55,34 +60,37 @@ void xx(vector<LL> &A, vector<LL> &x, int count)
 
 int main()
 {
-    //cin >> N;
-    N = 4;
+    cin >> N;
     vector<LL> A(N + 1);
     vector<LL> x(N + 1, 0);
-    /*
+
     for (int i = 0; i <= N; i++)
     {
         cin >> A.at(i);
     }
-    */
-    A.at(0) = 0;
-    A.at(1) = 0;
-    A.at(2) = 1;
-    A.at(3) = 0;
-    A.at(4) = 2;
 
     x.at(0) = 1;
     x.at(N) = 0;
 
-    xx(A, x, 1);
+    if (A.at(0) == 0 || (A.at(0) == 1 && N == 0))
+    {
+        xx(A, x, 1);
+    }
 
     LL ans = 0;
-    for (int i = 0; i <= N; i++)
+    if (check == 1)
     {
-        ans += x.at(i) + A.at(i);
-        cout << "深さ" << i << ':' << x.at(i) + A.at(i) << endl;
+        for (int i = 0; i <= N; i++)
+        {
+            ans += x.at(i) + A.at(i);
+            //cout << "深さ" << i << ':' << x.at(i) + A.at(i) << endl;
+        }
+        cout << ans << endl;
     }
-    cout << ans << endl;
+    else
+    {
+        cout << -1 << endl;
+    }
 
     return 0;
 }
